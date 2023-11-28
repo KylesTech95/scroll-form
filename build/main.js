921033a72b9807b1daa6ec0a0b535d5b6e4cd765
kylestech95 <kylestech95@outlook.com>
2023-11-27 21:35:14 -0500

    initial commit

---
+++ build/main.js
@@ -0,0 +1,112 @@
+let divs = document.querySelectorAll('.fm-div')
+divs.forEach((elem,i)=>{
+    let child = elem.children[0]
+    if(i!==divs.length-1){
+        if(i%2===0){
+            child.classList.add('float-right')
+        }
+        else{
+            child.classList.add('float-left')
+        }
+    }
+    else{
+        elem.classList.add('text-center')
+    }
+})
+let coll = (element,s) => {
+    element.classList.remove(s)
+}
+let exp = (element,s) => {
+    element.classList.add(s)
+}
+function collapse(arr){
+
+arr.forEach((el,idx)=>{
+    let float = el.children[0]
+    let input = [...float.children]
+    let filtered_inp = input.filter((_,i)=>i%2!==0)
+    let size = 'mt-80'
+    el.classList.add(size)
+    el.classList.add('duration-700')
+
+    //collapse & expand functions
+    filtered_inp.forEach((inp,i)=>{
+        inp.addEventListener('click',e=>{
+                e.target.classList.add('form-control-lg')
+        })
+        inp.addEventListener('focus',e=>{
+            if(e.target){
+                e.target.classList.add('form-control-lg')
+            }
+                
+        })
+
+        //change EL for collapsing/expanding 
+        inp.addEventListener('change',e =>{
+            let inc = 0, dec;
+            let drag = e.target.parentElement
+            let pos = e.target.getBoundingClientRect().y
+            if(e.target.value){
+                coll(el,size)
+            //code for dragging effect
+            while(inc < (pos/3)){
+                inc++
+                drag.style = `padding-bottom:${inc}px; transition:.5s ease;background-color:green; background-opacity:.5`
+                dec = inc;
+            }
+            while(dec >= 0){
+                dec--
+                setTimeout(()=>{
+                    drag.style = `padding-bottom:${dec}px; transition:.5s ease;`
+                },250)
+            }
+            }
+            else{
+                exp(el,size)
+            }
+            
+        })
+        //input EL for collapsing/expanding 
+        inp.addEventListener('input',e =>{
+            if(!e.target.value){
+                exp(el,size)
+            }
+            
+        })
+        
+         
+    })
+})
+window.addEventListener('click', e=>{
+    if(e.target.id === 'form'){
+    divs.forEach(div=>{
+        let float = div.children[0]
+        let arr = [...float.children]
+        arr = arr.filter((_,i)=>i%2!==0)
+        arr.forEach((inp,i)=>{
+            inp.classList.remove('form-control-lg')
+        })
+    })
+    }
+    else{
+        divs.forEach(div=>{
+            let float = div.children[0]
+            let arr = [...float.children]
+            arr = arr.filter((_,i)=>i%2!==0)
+            arr.forEach((inp,i)=>{
+                if(inp !== e.target){
+                    inp.classList.remove('form-control-lg')
+                    inp.style='transition:.25s ease;'
+                }
+            })
+        })
+    }
+})  
+
+
+
+}
+collapse(divs)
+
+
+
