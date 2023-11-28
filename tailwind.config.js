110c4243272be39eb0285a23561ff1b33cf9d93f
kylestech95 <kylestech95@outlook.com>
2023-11-27 21:50:50 -0500

    reverted back to beginning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix:'tw-',
-  important: true,
-  corePlugins: {
-    preflight: false,
-},
   content: ['./build/**/*.{html,js}'],
   theme: {
     extend: {},
